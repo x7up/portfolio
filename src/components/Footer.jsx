6de6dac52b1c3d91f43836c21551a38169f13ee6
x7up <53333883+x7up@users.noreply.github.com>
2024-10-29 00:18:11 +0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,7 +21,7 @@ function Footer() {
         <ul className="footer-icons">
       <li className="social-icons">
         <a
-          href="https://github.com/soumyajit4419"
+          href="https://github.com/x7up"
           style={{ color: "white" }}
           target="_blank"
           rel="noopener noreferrer"
