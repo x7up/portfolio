c56137663a2a1d8837a31caef3a3c6c6ea6fcc65
rajput-musa <musarajput256@outlook.com>
2024-10-29 00:44:07 +0500

    Final touch

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,7 +15,7 @@ function Footer() {
           <h3>Designed and Developed by Moosa Qaisar</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} SB</h3>
+          <h3>Copyright © {year} MO</h3>
         </Col>
         <Col md="4" className="footer-body">
         <ul className="footer-icons">
